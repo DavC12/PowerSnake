39bdb6800fbee30d304b392d4f7dad59e66bc057
Akzule <45741617+akzule@users.noreply.github.com>
2024-08-21 20:14:37 -0500

    Update main.js
    Re escalado de renderizador.

--- main.js
+++ main.js
@@ -2,20 +2,19 @@ import { Tail, Head } from "./modules/SnakeBody.js";
 
 // Funciones (requiere simplificación)
 function testing(location, block){
-   
-   if (ctx.getImageData(location.x, location.y, 1, 1).data.at(0) === block[0]){
+   if (ctx.getImageData(location.x * 10, location.y * 10, 1, 1).data.at(0) === block[0]){
       console.log("Fruto detectado.");
       return true;
    }
 }
-function setting(location, block){
-   
+
+function setting(location, block){ 
    if(block == "void"){
-      ctx.clearRect(location.x, location.y, 1, 1);
+      ctx.clearRect(location.x * 10, location.y * 10, 10, 10);
       console.log(`cola se ha movido hacia ${location.x}`)
    }
    else{
-      ctx.fillRect(location.x, location.y, 1, 1);
+      ctx.fillRect(location.x * 10, location.y * 10, 10, 10);
       console.log(`cabeza se ha movido hacia ${location.x}`)
    }
 }
@@ -24,13 +23,13 @@ function setting(location, block){
 // Declaraciones y DOM
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const head = new Head([255, 0], {x: 0, y: 0, z: 0}, "East", setting, testing);
+const head = new Head([255, 0], {x: 1, y: 1, z: 0}, "East", setting, testing);
 
 
 // Canvas
 ctx.fillStyle = "rgb(0 0 0)";
-ctx.fillRect(0, 0, 1, 1);
-// ctx.strokeRect(0, 0, 300, 150);
+ctx.fillRect(10, 10, 10, 10);
+ctx.strokeRect(0, 0, 300, 150);
 
 
 for(let i = 0; i < 10; i++){
